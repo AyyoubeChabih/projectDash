e2ffe49ab372f21c94e59db7054a27c9018ba00b
darbouz <47700312+darbouz@users.noreply.github.com>
2020-03-22 17:01:04 +0100

    update-sidebar

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import avatar from '../../assets/avatar.jpg';
+import './avatar.css';
+
+export const Avatar = () => {
+    return <img className='avatar' src={avatar} alt="avatar"/>
+};
+
+
--- src/components/Sidenav/index.js
+++ src/components/Sidenav/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './sidenav.css';
 import UserCard from './UserCard';
+import {Avatar} from "../Avatar";
 
 const Sidenav = () => {
     return (
         <div className="sidenav">
-            <UserCard></UserCard>
+            <div className='user-section'>
+                <Avatar/>
+                <div>
+                    <div className='username'>Amoine Daiki</div>
+                    <a className='settings'>settings <span>	&#129171;</span></a>
+                </div>
+            </div>
+
+            <ul>
+                <li>Dashboard</li>
+                <li className='active'>Portfolio</li>
+                <li>Programs</li>
+                <li>Projects</li>
+                <li>Metrics</li>
+            </ul>
+
         </div>
     );
 };
